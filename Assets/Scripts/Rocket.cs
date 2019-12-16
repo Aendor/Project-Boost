be8e36a4d2ae367a9b39c4f54ee41da2183b904f
Aendor <arnoutcschouten@hotmail.com>
2019-12-16 17:19:06 +0100

    PB 42 Complete

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessImput();
+    }
+
+    private void ProcessImput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // Thrust while rotating
+        {
+            print("Thrusting");
+        }
+
+        // Cannot rotate both ways at the same time
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotate Left");
+        } 
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotate Right");
+        }
+    }
+}
