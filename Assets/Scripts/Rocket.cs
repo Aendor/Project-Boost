6e5179aa4a1983d5021a6e383d49f7a48655201b
Aendor <arnoutcschouten@hotmail.com>
2019-12-17 12:13:00 +0100

    PB 49 Complete

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -8,6 +8,9 @@ public class Rocket : MonoBehaviour
     Rigidbody rigidBody;
     AudioSource audioSource;
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +28,8 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space)) // Thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+
             if (!audioSource.isPlaying) // So it doesnt layer
             {
                 audioSource.Play();
@@ -40,18 +44,19 @@ public class Rocket : MonoBehaviour
     {
         rigidBody.freezeRotation = true; //take manual control of rotation
 
+        //rcsThrust = 10f;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         // Cannot rotate both ways at the same time
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false; //resume physics control of rotation
     }
-
-    
 }
