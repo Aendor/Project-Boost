d068daa01401125fe797bf075514e6a738220007
Aendor <arnoutcschouten@hotmail.com>
2019-12-17 17:45:44 +0100

    PB 53 Complete

--- Assets/MovingObstacle.cs
+++ Assets/MovingObstacle.cs
@@ -9,15 +9,23 @@ public class MovingObstacle : MonoBehaviour
     [SerializeField] float speed = 10f;
     [SerializeField] float height = 10f;
 
+    [SerializeField] float WaitTime = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         pos = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        StartCoroutine(MyCoroutine());
+    }
+    IEnumerator MyCoroutine()
+    {
+        yield return new WaitForSeconds(WaitTime);    //Wait
         //calculate what the new Y position will be
         float newY = pos.y + Mathf.Sin(Time.time * speed) * height;
         //set the object's Y to the new calculated Y
